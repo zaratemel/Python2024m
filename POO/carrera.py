8d497903e53a10880484433a4942d94443258224
zaratemel <163873937+zaratemel@users.noreply.github.com>
2024-09-02 12:15:52 -0300

    Add files via upload

---
+++ POO/carrera.py
@@ -0,0 +1,6 @@
+from personaje import Personaje
+#nombre,altura,velocida,resistencia,fuerza
+
+p1 = Personaje("Batman", 180, 80, 90, 70)
+print(f"el personaje se llama {p1.nombre}")
+
---
+++ POO/personaje.py
@@ -0,0 +1,12 @@
+class Personaje:
+
+    estado = True 
+
+    def __init__(self,nombre,altura,velocida,resistencia,fuerza):
+
+        self.nombre = nombre
+        self.altura = altura
+        self.velocida = velocida
+        self.resistencia = resistencia
+        self.fuerza = fuerza
+
